c66413291acb2b4cb4f4059e60156f5be73f77d1
Ungergoo <47298342+Ungergoo@users.noreply.github.com>
2025-03-05 18:42:40 +0400

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,36 +1,30 @@
-# F(1) = 1, F(n) = F(n–1) * (3*n - 2), при n > 1
-
-
+# F(1) = 1, F(n) = ((-1)^n)*(F(n-1)+(n-2)!/(2n)!), при n > 1
 import time
 import matplotlib.pyplot as plt
+import math
+
 try:
-    # проверка числа на соответсвие условию задания
+    # Проверка числа на соответствие условию задания
     n = int(input("Введите число n > 1: "))
-    while n <= 2:
+    while n <= 1:
         n = int(input("Введите число n > 1: "))
 except ValueError:
-    print("Ввёденное вами значение n не является числом."
+    print("Введённое вами значение n не является числом."
           " Пожалуйста, перезапустите программу и введите корректное значение ")
 
 
 def F(n):
-    if n <= 1:
-        return 1
-    else:
-        return F(n - 1) * (3 * n - 2)
-
-
-def Iteratively(n):
     if n == 1:
         return 1
     else:
-        a = 1
-        for i in range(1, n + 1):
-            c = a * (3 * i - 2)
-            a = c
-        return c
+        return ((-1) ** n) * (F(n - 1) + (math.factorial(n - 2) / math.factorial(2 * n)))
 
 
+def Iteratively(n):
+    a = 1
+    for i in range(2, n + 1):
+        a = ((-1) ** i) * (a + (math.factorial(i - 2) / math.factorial(2 * i)))
+    return a
 
 
 start_time = time.time()
@@ -42,23 +36,22 @@ start_time = time.time()
 f_iter = Iteratively(n)
 end_time = time.time()
 iter_time = end_time - start_time
+
 print("F({}) = {} (рекурсивно в {:.6f} секунд)".format(n, f_rec, rec_time))
 print("F({}) = {} (итеративно в {:.6f} секунд)".format(n, f_iter, iter_time))
-
 
 rec_times = []
 rec_values = []
 iter_times = []
 iter_values = []
-n_values = list(range(1, n + 1, +1))
+n_values = list(range(1, n + 1))
 
 for n in n_values:
-    '''
     start = time.time()
     rec_values.append(F(n))
     end = time.time()
     rec_times.append(end - start)
-'''
+
     start = time.time()
     iter_values.append(Iteratively(n))
     end = time.time()
@@ -68,12 +61,11 @@ for n in n_values:
 table = []
 for i, n in enumerate(n_values):
     table.append([n, rec_times[i], iter_times[i], rec_values[i], iter_values[i]])
-print("{:<10}|{:<22}|{:<22}|{:<25}|{:<30}".format("n", "Время выполнения рекурсии (с)","Время выполнения итерации (с)","Значение рекурсии", "Значение итерации"))
 
+print("{:<10}|{:<22}|{:<22}|{:<25}|{:<30}".format("n", "Время выполнения рекурсии (с)", "Время выполнения итерации (с)", "Значение рекурсии", "Значение итерации"))
 print('-' * 160)
 for j in table:
     print("{:<10}|{:<22}|{:<22}|{:<25}|{:<30}".format(j[0], j[1], j[2], j[3], j[4]))
-
 
 plt.plot(n_values, rec_times, label="Рекурсия")
 plt.plot(n_values, iter_times, label="Итерация")
@@ -91,4 +83,3 @@ plt.legend()
 plt.show()
 
 print("\nРабота программы завершена.\n")
-
